c28e15693ac6d09c4a96c7d67cf6cff680232d1c
dan9b6 <dkburton0@gmail.com>
2020-02-24 15:35:02 +0000

    Edit profile operating successfully

--- routes/create-game.js
+++ routes/create-game.js
@@ -5,11 +5,9 @@ const router = new Router();
 
 const Game = require('../models/game');
 
-
 router.get('/:userId/create-game', (req, res, next) => {
   const { userId } = req.params;
   console.log(userId);
-  
 });
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -5,7 +5,6 @@ const router = new Router();
 
 const User = require('./../models/user');
 const uploader = require('./../middleware/upload');
-
 const routeGuard = require('./../middleware/route-guard');
 
 router.get('/:userId', (req, res, next) => {
@@ -15,6 +14,42 @@ router.get('/:userId', (req, res, next) => {
     console.log(data);
     res.render('profile', data);
   });
+});
+
+router.get('/:userId/edit', (req, res, next) => {
+  const { userId } = req.params;
+  //console.log(userId);
+  User.findById(userId).then(user => {
+    //console.log(data);
+    res.render('profile-edit', user);
+  });
+});
+
+//create middleware for image upload
+
+router.post('/:userId/edit', uploader.single('photo'), (req, res, next) => {
+  const userId = req.params.userId;
+  //console.log(req.file);
+  // const userId = req.user._id;
+  const { name, email, campus } = req.body;
+
+  let profilePicture;
+  if (req.file) {
+    profilePicture = req.file.url;
+  }
+
+  User.findByIdAndUpdate(userId, {
+    ...(name ? { name } : {}),
+    ...(email ? { email } : {}),
+    ...(campus ? { campus } : {}),
+    ...(profilePicture ? { profilePicture } : {})
+  })
+    .then(() => {
+      res.redirect(`/profile/${userId}`);
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;

ae75e1f35a37c837d68302fa5c21c3cd5090e704
Luca Campora <luca.campora@gmail.com>
2020-02-24 12:59:54 +0000

    game models and hbs done

---
+++ models/game.js
@@ -0,0 +1,40 @@
+'use strict';
+const mongoose = require('mongoose');
+const schemaGame = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 80,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 500,
+      trim: true
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    photos: 
+      {
+        type: String
+      },
+    tagline:
+    {
+      type: String,
+      maxlength: 70
+    } 
+  },
+  {
+    timestamps: {
+      createdAt: 'creationDate',
+      updatedAt: 'updateDate'
+    }
+  }
+);
+const Model = mongoose.model('Post', schemaGame);
+module.exports = Model;
---
+++ routes/create-game.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { Router } = require('express');
+const router = new Router();
+
+const Game = require('../models/game');
+
+
+router.get('/:userId/create-game', (req, res, next) => {
+  const { userId } = req.params;
+  console.log(userId);
+  
+});
+
+module.exports = router;

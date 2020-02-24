0f108edba50f1948962c90973c0999019aca9d87
dan9b6 <dkburton0@gmail.com>
2020-02-24 20:12:29 +0000

    Last commit of the day

--- routes/profile.js
+++ routes/profile.js
@@ -8,8 +8,11 @@ const uploader = require('./../middleware/upload');
 const routeGuard = require('./../middleware/route-guard');
 const Games = require('./../models/game.js');
 
+//Endpoint  = http://localhost:3000/profile/5e53f1c6adfa1053546d2972
+// req.params.userId gives you access the the user ID displayed in the url
 router.get('/:userId', (req, res, next) => {
-  const { userId } = req.params;
+  const userId = req.params.userId;
+  //This line 👆🏼 is equal to typing  const { userId } = req.params;
   let userInfo;
   User.findById(userId)
     .then(data => {
@@ -36,6 +39,7 @@ router.post('/:userId/edit', uploader.single('photo'), (req, res, next) => {
   const { name, email, campus } = req.body;
 
   let profilePicture;
+
   if (req.file) {
     profilePicture = req.file.url;
   }

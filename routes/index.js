461a431af6bb779a8d33b64b9c6d23248cb73ccc
dan9b6 <dkburton0@gmail.com>
2020-02-26 16:13:43 +0000

    Push for Sonia

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,24 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'codeIRON' });
 });
 
+router.get('/select', (req, res, next) => {
+  res.render('select');
+});
+
 router.get('/home', routeGuard, (req, res, next) => {
-  const userId = req.user._id
+  const userId = req.user._id;
   Projects.find()
     .then(projects => {
       console.log(projects);
-      projects.map(project =>{
+      projects.map(project => {
         if (userId.toString() === project.author.toString()) {
-          console.log("condition true");
-          project.sameUser = true
+          console.log('condition true');
+          project.sameUser = true;
         } else {
-          console.log("condition false");
-          project.sameUser = false
+          console.log('condition false');
+          project.sameUser = false;
         }
-      })
+      });
       res.render('home', { projects });
     })
     .catch(error => next(error));

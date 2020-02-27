26b9bc654bd83d12609f3bed7b4fee00242a691f
Luca Campora <luca.campora@gmail.com>
2020-02-27 13:23:33 +0000

    push

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const hbs = require('hbs');
 const app = express();
 
 app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs'); 
 
 hbs.registerPartials(join(__dirname, 'views/partials'));
 
